de551c67f6250af693ebfa8f4339ded85c000f57
Josh Waldron <jwaldron@thejwal.com>
2012-11-11 01:37:43 -0600

    added Remaining Controllers / views

---
+++ ComputeMidwest/Controllers/FinishedHuntController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComputeMidwest.Controllers
+{
+    public class FinishedHuntController : Controller
+    {
+        //
+        // GET: /FinishedHunt/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
