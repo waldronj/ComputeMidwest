3003f29ca99f77fd8047e77720de74948f2884fd
Josh Waldron <jwaldron@thejwal.com>
2012-11-11 02:55:45 -0600

    added stuff

--- ComputeMidwest/Controllers/CreateHuntController.cs
+++ ComputeMidwest/Controllers/CreateHuntController.cs
@@ -33,27 +33,28 @@ namespace ComputeMidwest.Controllers
         }
 
         [HttpPost]
-        public ActionResult Index(string HuntName)
+        public ActionResult Index(string huntName, string description)
         {
-            //HuntModel hm = new HuntModel(new EntityModelContainer(), new HuntNotifier(new PusherProvider("31452", "04af48f0bd881f9f9737", "0bbb6f45596775fa5d2d"))); 
-            
-            //AccountModel am = new AccountModel(new EntityModelContainer());
-            //Account act = am.GetAccountByAccountToken(Session["access_token"].ToString(), Session["account_type"].ToString());
-            //Hunt hunt = new Hunt()
-            //{
-            //    Name = huntName,
-            //    Description = description
-            //};
-            //hm.CreateHunt(act, hunt);
+            HuntModel hm = new HuntModel(new EntityModelContainer(), new HuntNotifier(new PusherProvider("31452", "04af48f0bd881f9f9737", "0bbb6f45596775fa5d2d")));
+
+            AccountModel am = new AccountModel(new EntityModelContainer());
+            Account act = am.GetAccountByAccountToken(Session["access_token"].ToString(), Session["account_type"].ToString());
+            Hunt hunt = new Hunt()
+            {
+                Creator = act,
+                Name = huntName,
+                Description = description
+            };
+            hm.CreateHunt(act, hunt);
             ComputeMidwest.Models.Communications comm = new Models.Communications();
 
             switch (Session["account_type"].ToString())
             {
                 case "Facebook":
-                    comm.PostToFacebook(HuntName, Session["access_token"].ToString());
+                    comm.PostToFacebook(huntName, Session["access_token"].ToString());
                     break;
                 case "Twitter":
-                    comm.PostToTwitter(HuntName, Session["access_token"].ToString());
+                    comm.PostToTwitter(huntName, Session["access_token"].ToString());
                     break;
 
             }
